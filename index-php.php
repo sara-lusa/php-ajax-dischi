cc5c63909b099f6e20d5448977bd416774d8ece1
Sara <saracorsob@gmail.com>
2020-07-16 15:51:51 +0200

    Aggiunta file base + inizio scrittura scss

---
+++ index-php.php
@@ -0,0 +1,28 @@
+<?php
+include __DIR__ . '/database.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+
+    <header>
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+    </header>
+
+    <main>
+
+    </main>
+
+  </body>
+</html>
